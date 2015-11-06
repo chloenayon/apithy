4619eb094b6f8d6ea224258b92157347480df3ca
Chloe Kaubisch <nemononiam@gmail.com>
2015-11-06 11:47:22 -0500

    asdkj

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import urllib2, json
+
+key = "dc81912032b4af7db369363784b553ee"
+
+joe = Flask(__name__)
+
+url = "api.openweathermap.org/data/2.5/weather?q=NYC"
+request = urllib2.urlopen(url)
+result = request.read()
+r = json.loads(result)
+print r
+
+@joe.route("/")
+def go():
+    pass
+
+if __name__=="__main__":
+    joe.debug = True
+    joe.run(host='0.0.0.0', port=8000)
