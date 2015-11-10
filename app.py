98541eb08d0c6398bd37c247384bd6c73bfc9b83
Chloe Kaubisch <nemononiam@gmail.com>
2015-11-09 22:54:15 -0500

    updates

--- app.py
+++ app.py
@@ -5,18 +5,16 @@ key = "dc81912032b4af7db369363784b553ee"
 
 joe = Flask(__name__)
 
-url = "http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=dc81912032b4af7db369363784b553ee"
+url = "http://api.openweathermap.org/data/2.5/weather?id=5128638&units=imperial&appid=dc81912032b4af7db369363784b553ee"
 request = urllib2.urlopen(url)
 result = request.read()
 r = json.loads(result)
 m = r["main"]["temp"]
-print translate(m)
-
-def translate(x):
-    return ((x * (9/5)) - 459.67)
+print m
 
 @joe.route("/")
 def go():
+#    return m
     pass
 
 if __name__=="__main__":
