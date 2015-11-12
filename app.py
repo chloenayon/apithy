99418136839b8e55c2e7c0dcb5523d18fcd6e098
Chloe Kaubisch <nemononiam@gmail.com>
2015-11-11 20:29:22 -0500

    updates

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 import urllib2, json
 
 key = "dc81912032b4af7db369363784b553ee"
 
 joe = Flask(__name__)
 
-url = "http://api.openweathermap.org/data/2.5/weather?id=5128638&units=imperial&appid=dc81912032b4af7db369363784b553ee"
-request = urllib2.urlopen(url)
-result = request.read()
-r = json.loads(result)
-m = r["main"]["temp"]
+#url = "http://api.openweathermap.org/data/2.5/weather?id=5128638&units=imperial&appid=dc81912032b4af7db369363784b553ee"
+#request = urllib2.urlopen(url)
+#result = request.read()
+#r = json.loads(result)
+#m = r["main"]["temp"]
 #print m
 
-event = """http://api.eventful.com/json/events/search?...&keywords=music&location=New+York&date=Future&app_key=rqSF6Lsh3t772JPf"""
-req = urllib2.urlopen(event)
-res = req.read()
-q = json.loads(res)
+#event = """http://api.eventful.com/json/events/search?...&keywords=music&location=New+York&date=Future&app_key=rqSF6Lsh3t772JPf"""
+#req = urllib2.urlopen(event)
+#res = req.read()
+#q = json.loads(res)
 #print q['events']['event'][2]['latitude']
 
 
-@joe.route("/")
+@joe.route("/", methods = ["GET", "POST"])
 def go():
-    return render_template('home.html')
+#    event = """http://api.eventful.com/json/events/search?...&keywords=music&location=New+York&date=Future&app_key=rqSF6Lsh3t772JPf"""
+#    req = urllib2.urlopen(event)
+#    res = req.read()
+#    q = json.loads(res)
+    #print q['events']['event'][2]['latitude']
+#    print request.form['search']
+    if request.method == "GET":
+        return render_template('home.html')
+    else:
+        print request.form['search']
+        return render_template('home.html')
 
 if __name__=="__main__":
     joe.debug = True
